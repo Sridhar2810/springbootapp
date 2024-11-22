71946bace6fe13a11a4fd56bb1432c1650a63d1a
Sridhar2810 <164170525+Sridhar2810@users.noreply.github.com>
2024-11-22 16:51:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "hello_world_app" {
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "allow_ssh_unique"
   description = "Allow SSH inbound traffic"
   vpc_id     = "vpc-0abcfde67c0317c28"
 
